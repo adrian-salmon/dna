64c10807d3bdc4459df350a800f0a2b70737f7a5
AdrianSalmonInteractives <adrian.salmon@ck12.org>
2025-03-18 16:04:43 -0700

    Update configuration and fix script loading

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ export default defineConfig({
   base: '/dna-inline/', // Repository name
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three']
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['three']
   }
 }) 
